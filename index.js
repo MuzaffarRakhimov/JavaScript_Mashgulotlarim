7ad5baa42135eefd1b88cca9a8150068affebcd5
MuzaffarRaximov <javascriptmernstack@gmail.com>
2023-07-30 11:00:57 +0300

    JS darsim_1

---
+++ index.js
@@ -0,0 +1,21 @@
+let salom='Assalomu aloykum mehmon';
+// let bu o'zgaruvchi;
+alert(salom);
+// alert () ogohlantirish , muhim xabarlarni elon qilinadi
+
+//____________--________________---------_______________________
+
+let muloqot='ismingiz kiriting';
+prompt(muloqot,'Muzaffar')
+//prompt () so'rov ,foydalanuvchidan malumot olish;
+
+//____________--________________---------_______________________
+
+let yosh ='yoshingiz 18 dan oshganmi..?';
+
+confirm(yosh);
+//confirm () tasdiqlash ,yani foydalanuvchidan malumotni tasdiqlasni so'laladi.
+
+
+
+
